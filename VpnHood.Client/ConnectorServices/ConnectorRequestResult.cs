423f5b0f909c23140fa50d9b88829ffc771d0532
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:15 +0900

    update file

---
+++ VpnHood.Client/ConnectorServices/ConnectorRequestResult.cs
@@ -0,0 +1,15 @@
+using VpnHood.Common.Messaging;
+using VpnHood.Tunneling.ClientStreams;
+
+namespace VpnHood.Client.ConnectorServices;
+
+internal class ConnectorRequestResult<T> : IAsyncDisposable where T : SessionResponseBase
+{
+    public required IClientStream ClientStream { get; init; }
+    public required T Response { get; init; }
+
+    public ValueTask DisposeAsync()
+    {
+        return ClientStream.DisposeAsync();
+    }
+}
