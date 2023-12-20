efadc21f78323bb30d7baa440c42dd1d8af49c5b
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:18 +0900

    update file

---
+++ VpnHood.Client/Exceptions/RedirectHostException.cs
@@ -0,0 +1,17 @@
+using System.Net;
+using VpnHood.Common.Exceptions;
+using VpnHood.Common.Messaging;
+
+namespace VpnHood.Client.Exceptions;
+
+public class RedirectHostException : SessionException
+{
+    public RedirectHostException(SessionResponseBase sessionResponseBaseBase)
+        : base(sessionResponseBaseBase)
+    {
+        if (sessionResponseBaseBase.RedirectHostEndPoint == null)
+            throw new ArgumentNullException(nameof(sessionResponseBaseBase.RedirectHostEndPoint));
+    }
+
+    public IPEndPoint RedirectHostEndPoint => SessionResponseBase.RedirectHostEndPoint!;
+}
