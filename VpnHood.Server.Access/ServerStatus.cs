db87da2f2a5901b61cd4ed70829f186fb9ea7e03
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:50 +0900

    update file

---
+++ VpnHood.Server.Access/ServerStatus.cs
@@ -0,0 +1,17 @@
+using VpnHood.Common.Messaging;
+
+namespace VpnHood.Server.Access;
+
+public class ServerStatus
+{
+    public int SessionCount { get; set; }
+    public int TcpConnectionCount { get; set; }
+    public int UdpConnectionCount { get; set; }
+    public long? AvailableMemory { get; set; }
+    public int? CpuUsage { get; set; }
+    public long UsedMemory { get; set; }
+    public int ThreadCount { get; set; }
+    public Traffic TunnelSpeed { get; set; } = new();
+    public string? ConfigCode { get; set; }
+    public string? ConfigError { get; set; }
+}
