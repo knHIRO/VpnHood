3aa7562512d8bf5c75b27f5cb5b2ea59a72084b9
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:07 +0900

    update file

---
+++ VpnHood.Server/Exceptions/TlsAuthenticateException.cs
@@ -0,0 +1,9 @@
+namespace VpnHood.Server.Exceptions;
+
+internal class TlsAuthenticateException : Exception
+{
+    public TlsAuthenticateException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
