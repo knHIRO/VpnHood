05a82c6d2f173b3f738ffe135d0071d2a61bd651
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:45 +0900

    update file

---
+++ VpnHood.Server/ServerUdpChannelTransmitter.cs
@@ -0,0 +1,28 @@
+using System.Net;
+using System.Net.Sockets;
+using VpnHood.Tunneling.Channels;
+
+namespace VpnHood.Server;
+
+public class ServerUdpChannelTransmitter : UdpChannelTransmitter
+{
+    private readonly SessionManager _sessionManager;
+
+    public ServerUdpChannelTransmitter(UdpClient udpClient, SessionManager sessionManager)
+        : base(udpClient, sessionManager.ServerSecret)
+    {
+        _sessionManager = sessionManager;
+    }
+
+    protected override void OnReceiveData(ulong sessionId, IPEndPoint remoteEndPoint,
+        long channelCryptorPosition, byte[] buffer, int bufferIndex)
+    {
+        var session = _sessionManager.GetSessionById(sessionId) 
+            ?? throw new Exception($"Session does not found. SessionId: {sessionId}");
+
+        //make sure UDP channel is added
+        session.UseUdpChannel = true;
+        session.UdpChannel?.SetRemote(this, remoteEndPoint);
+        session.UdpChannel?.OnReceiveData(channelCryptorPosition, buffer, bufferIndex);
+    }
+}
