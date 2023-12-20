b3668f463992b930c392d754431d20787c0c93b7
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:39:28 +0900

    update file

---
+++ VpnHood.Server.Access/Managers/Http/HttpAccessManagerOptions.cs
@@ -0,0 +1,15 @@
+namespace VpnHood.Server.Access.Managers.Http;
+
+public class HttpAccessManagerOptions
+{
+    public HttpAccessManagerOptions(Uri baseUrl, string authorization)
+    {
+        if (string.IsNullOrEmpty(authorization)) throw new ArgumentNullException(nameof(authorization));
+
+        BaseUrl = baseUrl;
+        Authorization = authorization;
+    }
+
+    public Uri BaseUrl { get; set; }
+    public string Authorization { get; set; }
+}
