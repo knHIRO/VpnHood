47b62a86fea5bb6e6166cb6ba0ffae76caf7d2ab
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:56 +0900

    update file

---
+++ VpnHood.Common/Messaging/SessionResponse.cs
@@ -0,0 +1,26 @@
+using System.Text.Json.Serialization;
+
+namespace VpnHood.Common.Messaging;
+
+public class SessionResponse : SessionResponseBase
+{
+    [JsonConstructor]
+    public SessionResponse(SessionErrorCode errorCode)
+        : base(errorCode)
+    {
+    }
+
+    public SessionResponse(SessionResponse obj)
+        : base(obj)
+    {
+        SessionId = obj.SessionId;
+        SessionKey = obj.SessionKey;
+        CreatedTime = obj.CreatedTime;
+        SuppressedTo = obj.SuppressedTo;
+    }
+
+    public ulong SessionId { get; set; }
+    public byte[] SessionKey { get; set; } = Array.Empty<byte>();
+    public DateTime? CreatedTime { get; set; }
+    public SessionSuppressType SuppressedTo { get; set; }
+}
