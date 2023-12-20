e64bb2958e87f316d4488d28d2544aefdc6939ff
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:15 +0900

    update file

---
+++ VpnHood.Client.App.Android/App.cs
@@ -0,0 +1,27 @@
+using Android.Runtime;
+using VpnHood.Client.App.Droid.Common;
+using VpnHood.Client.App.Droid.Properties;
+using VpnHood.Client.App.Resources;
+
+namespace VpnHood.Client.App.Droid;
+
+[Application(
+    Label = "@string/app_name",
+    Icon = "@mipmap/appicon",
+    Banner = "@mipmap/banner", // for TV
+    UsesCleartextTraffic = true, // required for localhost
+    SupportsRtl = true, AllowBackup = true)]
+public class App : AndroidApp
+{
+    public App(IntPtr javaReference, JniHandleOwnership transfer)
+        : base(javaReference, transfer)
+    {
+    }
+
+    protected override AppOptions AppOptions => new()
+    {
+        UpdateInfoUrl = AssemblyInfo.UpdateInfoUrl,
+        Resources = VpnHoodAppResource.Resources,
+    };
+
+}
