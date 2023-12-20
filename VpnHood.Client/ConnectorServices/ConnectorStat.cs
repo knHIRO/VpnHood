6e96eef69fe2013d91443de5cc86ebc8bce3cb2a
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:16 +0900

    update file

---
+++ VpnHood.Client/ConnectorServices/ConnectorStat.cs
@@ -0,0 +1,11 @@
+namespace VpnHood.Client.ConnectorServices;
+
+public abstract class ConnectorStat
+{
+    public abstract int FreeConnectionCount { get; } 
+    public int ReusedConnectionFailedCount { get; internal set; }
+    public int ReusedConnectionSucceededCount { get; internal set; }
+    public int CreatedConnectionCount { get; internal set; }
+    public int RequestCount { get; internal set; }
+
+}
