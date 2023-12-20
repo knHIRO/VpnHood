709463ff5c6e6b9db271801c811246b5e313dc54
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:57 +0900

    update file

---
+++ VpnHood.Common/Messaging/SessionSuppressType.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+
+namespace VpnHood.Common.Messaging;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum SessionSuppressType
+{
+    None,
+    YourSelf,
+    Other
+}
