7fcafb99a7897c8d7616849f524519ac55f13c27
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:13 +0900

    update file

---
+++ VpnHood.Client.App/PublishInfo.cs
@@ -0,0 +1,12 @@
+namespace VpnHood.Client.App;
+
+public class PublishInfo
+{
+    public required Version Version { get; init;}
+    public required Uri UpdateInfoUrl { get; init; }
+    public required Uri PackageUrl { get; init; }
+    public required Uri InstallationPageUrl { get; init; }
+    public required DateTime ReleaseDate { get; init; }
+    public required Version DeprecatedVersion { get; init; }
+    public required TimeSpan NotificationDelay { get; init; }
+}
