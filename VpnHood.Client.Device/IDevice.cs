614f2b2ee1d0ae33936d6e1d38f164fc3dce4ef5
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:35 +0900

    update file

---
+++ VpnHood.Client.Device/IDevice.cs
@@ -0,0 +1,11 @@
+namespace VpnHood.Client.Device;
+
+public interface IDevice
+{
+    string OperatingSystemInfo { get; }
+    DeviceAppInfo[] InstalledApps { get; }
+    bool IsExcludeAppsSupported { get; }
+    bool IsIncludeAppsSupported { get; }
+    event EventHandler OnStartAsService;
+    Task<IPacketCapture> CreatePacketCapture();
+}
