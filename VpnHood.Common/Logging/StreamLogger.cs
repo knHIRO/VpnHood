2498b334dbf1fc8bbf7b4f4bc94b94c925caecc4
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:53 +0900

    update file

---
+++ VpnHood.Common/Logging/StreamLogger.cs
@@ -0,0 +1,35 @@
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace VpnHood.Common.Logging;
+
+public class StreamLogger : TextLogger
+{
+    private const int DefaultBufferSize = 1024;
+    private readonly StreamWriter _streamWriter;
+
+    public StreamLogger(Stream stream, bool includeScopes = true, bool leaveOpen = false)
+        : base(includeScopes)
+    {
+        _streamWriter = new StreamWriter(stream, Encoding.UTF8, DefaultBufferSize, leaveOpen);
+    }
+
+    public override void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        var text = FormatLog(logLevel, eventId, state, exception, formatter);
+        _streamWriter.WriteLine(text);
+    }
+
+    public override void Dispose()
+    {
+        try { _streamWriter.Flush(); }
+        catch
+        {
+            // ignored
+        }
+
+        _streamWriter.Dispose();
+        base.Dispose();
+    }
+}
