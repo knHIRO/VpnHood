0b3b76c80f2f4f3460a8a886d982588b6572b09d
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:09 +0900

    update file

---
+++ Tests/VpnHood.Test/TestAppProvider.cs
@@ -0,0 +1,15 @@
+using VpnHood.Client.App;
+using VpnHood.Client.Device;
+
+namespace VpnHood.Test;
+
+internal class TestAppProvider : IAppProvider
+{
+    public TestAppProvider(TestDeviceOptions? testDeviceOptions)
+    {
+        Device = TestHelper.CreateDevice(testDeviceOptions);
+    }
+
+    public IDevice Device { get; }
+    public bool IsLogToConsoleSupported => true;
+}
