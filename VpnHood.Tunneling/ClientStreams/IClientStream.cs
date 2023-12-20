258116923e6ddeaf1ee8a0b1dfe438216f8ee1b6
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:18 +0900

    update file

---
+++ VpnHood.Tunneling/ClientStreams/IClientStream.cs
@@ -0,0 +1,13 @@
+using VpnHood.Common.Net;
+
+namespace VpnHood.Tunneling.ClientStreams;
+
+public interface IClientStream : IAsyncDisposable
+{
+    bool Disposed { get; }
+    string ClientStreamId { get; set; }
+    IPEndPointPair IpEndPointPair { get; }
+    Stream Stream { get; }
+    bool CheckIsAlive();
+    ValueTask DisposeAsync(bool graceful = true);
+}
