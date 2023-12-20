3e50d3327ea2098e502c9d4e4b705ddeee3149ec
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:20 +0900

    update file

---
+++ VpnHood.Tunneling/Exceptions/UdpClientQuotaException.cs
@@ -0,0 +1,17 @@
+using VpnHood.Common.Logging;
+
+namespace VpnHood.Tunneling.Exceptions;
+
+public class UdpClientQuotaException : Exception, ISelfLog
+{
+    public UdpClientQuotaException(int maxUdpClient) 
+        : base($"Maximum UdpClient has been reached. MaxUdpClient: {maxUdpClient}")
+    {
+
+    }
+
+    public void Log()
+    {
+        // do nothing
+    }
+}
