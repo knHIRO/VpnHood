80eb83aed46055409a88e0eded376236776f96d9
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:52 +0900

    update file

---
+++ VpnHood.Common/Logging/FilterLogger.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Logging;
+
+namespace VpnHood.Common.Logging;
+
+public class FilterLogger : ILogger
+{
+    private readonly Func<EventId, bool> _eventFilter;
+    private readonly ILogger _logger;
+
+    public FilterLogger(ILogger logger, Func<EventId, bool> eventFilter)
+    {
+        _logger = logger;
+        _eventFilter = eventFilter;
+    }
+
+    public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+    {
+        return _logger.BeginScope(state);
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return _logger.IsEnabled(logLevel);
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception,
+        Func<TState, Exception?, string> formatter)
+    {
+        if (_eventFilter(eventId))
+            _logger.Log(logLevel, eventId, state, exception, formatter);
+    }
+}
