998384f7afa64ed396c7f7ea54008a85aee553a6
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:11 +0900

    update file

---
+++ VpnHood.Server.Access/Messaging/SessionRequestEx.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Text.Json.Serialization;
+using VpnHood.Common.Converters;
+using VpnHood.Common.Messaging;
+
+namespace VpnHood.Server.Access.Messaging;
+
+public class SessionRequestEx : SessionRequest
+{
+    [JsonConstructor]
+    public SessionRequestEx(string requestId, Guid tokenId, ClientInfo clientInfo, byte[] encryptedClientId, IPEndPoint hostEndPoint)
+        : base(0, requestId, tokenId, clientInfo, encryptedClientId)
+    {
+        HostEndPoint = hostEndPoint;
+    }
+
+    public SessionRequestEx(SessionRequest obj, IPEndPoint hostEndPoint)
+        : base(obj)
+    {
+        HostEndPoint = hostEndPoint;
+    }
+
+    [JsonConverter(typeof(IPEndPointConverter))]
+    public IPEndPoint HostEndPoint { get; set; }
+
+    [JsonConverter(typeof(IPAddressConverter))]
+    public IPAddress? ClientIp { get; set; }
+
+    public string? ExtraData { get; set; }
+}
