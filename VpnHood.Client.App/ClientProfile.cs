8c40bf33e3b629794cf61995e0d7198979969d60
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:10 +0900

    update file

---
+++ VpnHood.Client.App/ClientProfile.cs
@@ -0,0 +1,8 @@
+namespace VpnHood.Client.App;
+
+public class ClientProfile
+{
+    public string? Name { get; set; }
+    public Guid ClientProfileId { get; set; }
+    public Guid TokenId { get; set; }
+}
