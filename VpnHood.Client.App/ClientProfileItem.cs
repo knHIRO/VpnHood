30beb9f6aba88ee63aec8740280d2f63509fd398
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:10 +0900

    update file

---
+++ VpnHood.Client.App/ClientProfileItem.cs
@@ -0,0 +1,11 @@
+using VpnHood.Common;
+
+namespace VpnHood.Client.App;
+
+public class ClientProfileItem
+{
+    public Guid ClientProfileId => ClientProfile.ClientProfileId;
+    public required ClientProfile ClientProfile { get; init; }
+    public required Token Token { get; init; }
+    public string? Name => string.IsNullOrWhiteSpace(ClientProfile.Name) ? Token.Name : ClientProfile.Name;
+}
