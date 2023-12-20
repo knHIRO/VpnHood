39db78df18c0551c1977d77fd74197f226a92295
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:18 +0900

    update file

---
+++ VpnHood.Client/Exceptions/NoStableVpnException.cs
@@ -0,0 +1,8 @@
+namespace VpnHood.Client.Exceptions;
+
+public class NoStableVpnException : Exception
+{
+    public NoStableVpnException() : base("VPN was connected, but it looked like the connection was not stable.")
+    {
+    }
+}
