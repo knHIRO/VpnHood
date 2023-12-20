a4a8a73d9845b82ba4037dcc71297a04510d821e
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:41 +0900

    update file

---
+++ VpnHood.Common/CommandReceivedEventArgs.cs
@@ -0,0 +1,10 @@
+namespace VpnHood.Common;
+
+public class CommandReceivedEventArgs : EventArgs
+{
+    public string[] Arguments;
+    public CommandReceivedEventArgs(string[] arguments)
+    {
+        Arguments = arguments;
+    }
+}
