b63f4ea431ae81e5601049b530ab12ab2570ebac
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:53 +0900

    update file

---
+++ VpnHood.Server.App.Net/Program.cs
@@ -0,0 +1,17 @@
+namespace VpnHood.Server.App;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        try
+        {
+            using var serverApp = new ServerApp();
+            await serverApp.Start(args);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
