6a0ea5902544ec0a278326fd02cfc8f014df1889
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:36 +0900

    update file

---
+++ VpnHood.Client.App.WebServer/Api/IClientApi.cs
@@ -0,0 +1,22 @@
+using VpnHood.Client.App.Settings;
+using VpnHood.Client.Device;
+
+namespace VpnHood.Client.App.WebServer.Api;
+
+public interface IClientApi
+{
+    Task<AppConfig> GetConfig();
+    Task<AppState> GetState();
+    Task Connect(Guid? clientProfileId = null);
+    Task Diagnose(Guid? clientProfileId = null);
+    Task Disconnect();
+    Task<ClientProfile> AddAccessKey(string accessKey);
+    Task UpdateClientProfile(Guid clientProfileId, ClientProfileUpdateParams updateParams);
+    Task DeleteClientProfile(Guid clientProfileId);
+    void ClearLastError();
+    void AddTestServer();
+    Task SetUserSettings(UserSettings userSettings);
+    Task<string> Log();
+    Task<DeviceAppInfo[]> GetInstalledApps();
+    Task<IpGroup[]> GetIpGroups();
+}
