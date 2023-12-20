1b76d68a61fb597a93c6cf6eb0be3e1ae4ebfd09
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:36 +0900

    update file

---
+++ VpnHood.Client.Device/PacketReceivedEventArgs.cs
@@ -0,0 +1,15 @@
+using PacketDotNet;
+
+namespace VpnHood.Client.Device;
+
+public class PacketReceivedEventArgs : EventArgs
+{
+    public IPPacket[] IpPackets { get; }
+    public IPacketCapture PacketCapture { get; }
+
+    public PacketReceivedEventArgs(IPPacket[] ipPackets, IPacketCapture packetCapture)
+    {
+        IpPackets = ipPackets;
+        PacketCapture = packetCapture;
+    }
+}
