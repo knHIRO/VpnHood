aaa336af3e019c789ec2d90dd948fcc95b322a53
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:09 +0900

    update file

---
+++ Tests/VpnHood.Test/TestDevice.cs
@@ -0,0 +1,31 @@
+using VpnHood.Client.Device;
+
+namespace VpnHood.Test;
+
+internal class TestDevice : IDevice
+{
+    private readonly TestDeviceOptions _options;
+    public TestDevice(TestDeviceOptions? options = default)
+    {
+        _options = options ?? new TestDeviceOptions();
+    }
+
+#pragma warning disable 0067
+    public event EventHandler? OnStartAsService;
+#pragma warning restore 0067
+
+    public string OperatingSystemInfo =>
+        Environment.OSVersion + ", " + (Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit");
+
+    public bool IsExcludeAppsSupported => false;
+
+    public bool IsIncludeAppsSupported => false;
+
+    public DeviceAppInfo[] InstalledApps => throw new NotSupportedException();
+
+    public Task<IPacketCapture> CreatePacketCapture()
+    {
+        var res = new TestPacketCapture(_options);
+        return Task.FromResult((IPacketCapture) res);
+    }
+}
