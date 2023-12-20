f8fdf602ece8049e92802b401b05e2065ca34429
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:06 +0900

    update file

---
+++ VpnHood.Server/Exceptions/RequestBlockedException.cs
@@ -0,0 +1,22 @@
+using System.Net;
+using Microsoft.Extensions.Logging;
+using VpnHood.Common.Messaging;
+using VpnHood.Tunneling;
+
+namespace VpnHood.Server.Exceptions;
+
+internal class RequestBlockedException : ServerSessionException
+{
+    public RequestBlockedException(IPEndPoint remoteEndPoint, Session session, string requestId)
+        : base(remoteEndPoint, session, SessionErrorCode.GeneralError, requestId, "The destination address is blocked.")
+    {
+    }
+
+    protected override LogLevel LogLevel => LogLevel.Information;
+    protected override EventId EventId => GeneralEventId.NetFilter;
+    
+    public override void Log()
+    {
+        //let EventReporter manage it
+    }
+}
