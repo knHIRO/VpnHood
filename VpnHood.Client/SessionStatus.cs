d460939a511ea7f79a8a6ca2fc3440fb04152d81
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:05 +0900

    update file

---
+++ VpnHood.Client/SessionStatus.cs
@@ -0,0 +1,12 @@
+using VpnHood.Common.Messaging;
+
+namespace VpnHood.Client;
+
+public class SessionStatus
+{
+    public SessionErrorCode ErrorCode { get; internal set; }
+    public AccessUsage? AccessUsage { get; internal set; }
+    public SessionSuppressType SuppressedTo { get; internal set; }
+    public SessionSuppressType SuppressedBy { get; internal set; }
+    public string? ErrorMessage { get; internal set; }
+}
