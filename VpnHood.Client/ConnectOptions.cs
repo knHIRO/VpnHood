cdab41724dab0d809e001b0568bf3b8cbd432c2f
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:36:04 +0900

    update file

---
+++ VpnHood.Client/ConnectOptions.cs
@@ -0,0 +1,13 @@
+namespace VpnHood.Client;
+
+public class ConnectOptions
+{
+    public int MaxReconnectCount { get; set; } = 3;
+
+    /// <summary>
+    ///     Time in millisecond
+    /// </summary>
+    public TimeSpan ReconnectDelay { get; set; } = TimeSpan.FromSeconds(20);
+
+    public UdpChannelMode UdpChannelMode { get; set; } = UdpChannelMode.Off;
+}
