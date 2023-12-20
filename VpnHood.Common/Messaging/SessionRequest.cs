4455ded0ec22668a92b9c8e1f996fcde0a5d64f6
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:56 +0900

    update file

---
+++ VpnHood.Common/Messaging/SessionRequest.cs
@@ -0,0 +1,27 @@
+using System.Text.Json.Serialization;
+
+namespace VpnHood.Common.Messaging;
+
+public abstract class SessionRequest : ClientRequest
+{
+    [JsonConstructor]
+    protected SessionRequest(byte requestCode, string requestId, Guid tokenId, ClientInfo clientInfo, byte[] encryptedClientId)
+        : base(requestCode, requestId)
+    {
+        TokenId = tokenId;
+        ClientInfo = clientInfo ?? throw new ArgumentNullException(nameof(clientInfo));
+        EncryptedClientId = encryptedClientId ?? throw new ArgumentNullException(nameof(encryptedClientId));
+    }
+
+    protected SessionRequest(SessionRequest obj)
+    : base(obj)
+    {
+        TokenId = obj.TokenId;
+        ClientInfo = obj.ClientInfo;
+        EncryptedClientId = obj.EncryptedClientId;
+    }
+
+    public Guid TokenId { get; init; }
+    public ClientInfo ClientInfo { get; init; }
+    public byte[] EncryptedClientId { get; init; }
+}
