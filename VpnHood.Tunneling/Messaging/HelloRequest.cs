3ef735480bfee636f0450e7df37279f5d1854a91
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:22 +0900

    update file

---
+++ VpnHood.Tunneling/Messaging/HelloRequest.cs
@@ -0,0 +1,15 @@
+using VpnHood.Common.Messaging;
+
+namespace VpnHood.Tunneling.Messaging;
+
+public class HelloRequest : SessionRequest
+{
+    public HelloRequest(string requestId, Guid tokenId, ClientInfo clientInfo, byte[] encryptedClientId)
+        : base((byte)Messaging.RequestCode.Hello, requestId,  tokenId, clientInfo, encryptedClientId)
+    {
+    }
+
+    //todo deprecated >= 416
+    [Obsolete("Not needed for server >= 416")]
+    public bool UseUdpChannel2 { get; set; } = true; 
+}
