349d72c27c91c95890018df021208a16ff0d16b7
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:47 +0900

    update file

---
+++ VpnHood.Common/Exceptions/SessionException.cs
@@ -0,0 +1,19 @@
+using VpnHood.Common.Messaging;
+
+namespace VpnHood.Common.Exceptions;
+
+public class SessionException : Exception
+{
+    public SessionException(SessionResponseBase sessionResponseBase)
+        : base(sessionResponseBase.ErrorMessage)
+    {
+        SessionResponseBase = sessionResponseBase;
+    }
+
+    public SessionException(SessionErrorCode errorCode, string? message = null) : base(message)
+    {
+        SessionResponseBase = new SessionResponseBase(errorCode) { ErrorMessage = message };
+    }
+
+    public SessionResponseBase SessionResponseBase { get; }
+}
