b1b78ecb1f70bbeb0ddd052fae4bc6e0420e9ae5
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:38:23 +0900

    update file

---
+++ VpnHood.Tunneling/Messaging/RequestCode.cs
@@ -0,0 +1,12 @@
+namespace VpnHood.Tunneling.Messaging;
+// [1B_version][1B_code][4B_jsonLength][json_request]
+
+public enum RequestCode : byte 
+{
+    Hello = 1, 
+    TcpDatagramChannel = 2,
+    StreamProxyChannel = 3,
+    // SessionStatus = 4,
+    UdpPacket = 5,
+    Bye = 50,
+}
