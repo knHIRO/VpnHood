548c292bdeb750f1b06fec688eb39f4eaffe420c
knHIRO <113556726+knHIRO@users.noreply.github.com>
2023-12-21 01:37:54 +0900

    update file

---
+++ VpnHood.Common/Messaging/AccessUsage.cs
@@ -0,0 +1,10 @@
+namespace VpnHood.Common.Messaging;
+
+public class AccessUsage
+{
+    public Traffic Traffic { get; set; } = new ();
+    public long MaxTraffic { get; set; }
+    public DateTime? ExpirationTime { get; set; }
+    public int MaxClientCount { get; set; }
+    public int? ActiveClientCount { get; set; }
+}
